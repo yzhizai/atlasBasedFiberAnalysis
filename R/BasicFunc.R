4621e2ea9914ea593834c03964369c568e5b3d22
yzhizai <dsf_ihep@yeah.net>
2016-06-19 22:09:43 +0800

    export FDcalc in namespace

--- R/BasicFunc.R
+++ R/BasicFunc.R
@@ -2,7 +2,8 @@
 #'
 #' @param df1 a dataframe with column called FA
 #' @param df2 a dataframe with column called FA
-#' @return The FD value
+#' @return return the FD value of \code{df1$FA} and \code{df2$FA}
+#' @export
 FDcalc <- function(df1, df2) {
   FDvalue <- sum(abs(df1$FA-df2$FA))/nrow(df1)
 }
